bb5b0d31c3e6c5c1bfeded3b61550c5a71106aa9
Timothy Ojo <oluwaseyitimothy99@gmail.com>
2025-08-09 19:50:32 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/word_hurdle_page.dart';
+ import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/hurdle_provider.dart';
+void main() {
+  runApp(ChangeNotifierProvider(
+      create: (context) => HurdleProvider(),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      builder: EasyLoading.init(),
+      home: const WordHurdlePage(),
+    );
+  }
+}
+
